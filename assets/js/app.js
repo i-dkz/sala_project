165eaebe15ea0c7f9534f3956670fdf6dc7f0a20
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-07-22 00:33:47 -0700

    updated a bunch of things, form, modal, cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,44 @@ hamburger.addEventListener('click', () => {
 document.getElementById('apply').addEventListener('click', function() {
   document.getElementById('registration').classList.remove('hidden');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modalOverlay = document.getElementById("modalOverlay");
+  const modalContent = document.getElementById("modalContent");
+  const registrationForm = document.getElementById("registration");
+  const closeButton = document.querySelector(".close-button");
+
+  function showModal(content) {
+    modalContent.innerHTML = content;
+    modalOverlay.style.display = "block";
+  }
+
+  function hideModal() {
+    modalOverlay.style.display = "none";
+    registrationForm.reset();
+  }
+
+  function hideForm() {
+    document.getElementById('registration').classList.add('hidden');
+  }
+
+  registrationForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById("full-name").value;
+    const email = document.getElementById("email").value;
+
+    const modalContentText = `<p>Thank you for booking, ${fullName}!</p>
+          <br>
+          <p>You will receive a confirmation email at ${email}</p>
+          <br>`;
+
+    showModal(modalContentText);
+  });
+
+  closeButton.addEventListener("click", function() {
+    hideModal();
+    hideForm();
+  });
+
+});
