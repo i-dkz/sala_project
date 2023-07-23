5b920fe3b7511ee12bf91e6d812f2b148dd4a54a
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-07-23 15:13:58 -0700

    fixed hamburger, sizing, etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,18 @@
-// const primaryNav = document.querySelector('.mobile-nav')
-// const hamburger = document.querySelector('.hamburger')
-
-// hamburger.addEventListener('click', () => {
-//     hamburger.classList.toggle('active')
-//     const visibility = primaryNav.getAttribute('data-visible')
-
-//     if (visibility === "false") {
-//       primaryNav.setAttribute("data-visible", true);
-//       hamburger.setAttribute("aria-expanded",true);
-//     } else {
-//       primaryNav.setAttribute("data-visible", false);
-//       hamburger.setAttribute("aria-expanded", false);
-//     }
-// }) 
-
-// primaryNav.addEventListener('click', function() {
-//   primaryNav.setAttribute("data-visible", false);
-//   hamburger.setAttribute("aria-expanded", false);
-//   hamburger.classList.toggle('active')
-// });
-
-const hamburger = document.querySelector('.hamburger');
-let hamburgerOpen = false;
 const primaryNav = document.querySelector('.mobile-nav')
+const hamburger = document.querySelector('.hamburger')
 
 hamburger.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible')
-  if (!hamburgerOpen) {
-    hamburger.classList.add('open');
-    hamburgerOpen = true;
-    primaryNav.setAttribute("data-visible", true);
-  } else {
-    hamburger.classList.remove('open');
-    hamburgerOpen = false;
-    primaryNav.setAttribute("data-visible", false);
-  }
-});
+    hamburger.classList.toggle('active')
+    const visibility = primaryNav.getAttribute('data-visible')
+
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", true);
+      hamburger.setAttribute("aria-expanded",true);
+    } else {
+      primaryNav.setAttribute("data-visible", false);
+      hamburger.setAttribute("aria-expanded", false);
+    }
+}) 
 
 document.getElementById('apply').addEventListener('click', function() {
   document.getElementById('registration').classList.remove('hidden');
